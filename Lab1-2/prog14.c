4db7a232aab902df7ca2733bdf1f4e5d315adbe0
sumanthreddy07 <kota.sumanthreddy@gmail.com>
2020-06-19 15:09:35 +0530

    clearing files

--- Lab1-2/prog14.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-int main(int argc,char *argv[])
-{
-    printf("Back to example.c\n");
-    printf("PID of hello.c = %d\n", getpid());
-    return 0;
-}
